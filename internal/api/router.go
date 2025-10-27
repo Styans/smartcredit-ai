1fee1cc5cf09f1287c261daa9d5d16076ffc726c
styan <milkaxd657@gmail.com>
2025-10-28 02:37:14 +0500

    fix(config): Explicitly bind environment variables for Render

--- internal/api/router.go
+++ internal/api/router.go
@@ -19,7 +19,9 @@ func SetupRouter(db *gorm.DB, cfg *config.Config) *gin.Engine {
 
 	// ... (Настройка CORS) ...
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
